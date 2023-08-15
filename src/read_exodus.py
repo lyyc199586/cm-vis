0d07e9863d753fef578c44bc62654d4bf11766dd
Yangyaunchen Liu <liuyangyuanchen@outlook.com>
2023-08-15 14:51:41 -0400

    add support to quad mesh and elemental variable

--- src/read_exodus.py
+++ src/read_exodus.py
@@ -1,6 +1,6 @@
 # template script to read exodus file and plot variable
 
-# %%
+# %% test for results on traingle mesh
 from utils.exodus import Exodus
 from utils.post import FEMPlotter
 
@@ -10,9 +10,33 @@ exodus = Exodus(file_dir)
 # read exodus file
 t = exodus.get_time()
 verts, faces = exodus.get_mesh()
-d = exodus.get_var(var_name="d", timestep=-1)
+d = exodus.get_var(var_name="d", timestep=30) # test nodal var
+psie_active = exodus.get_var(var_name="psie_active", timestep=30) # test elemental var
 
-# plot variable
-plotter = FEMPlotter(verts, faces, d)
-ax = plotter.plot(dim=2)
+# plot mesh, nodal variable, and elemental variable
+plotter = FEMPlotter(verts, faces)
+plotter.plot(lw=0.1)
+plotter.plot(d)
+plotter.plot(psie_active)
+
+# %% test for results on quad mesh
+
+from utils.exodus import Exodus
+from utils.post import FEMPlotter
+
+file_dir = "../../temp/brz_nuc22_p300_a10_l2.5_d5_iref2.e"
+exodus = Exodus(file_dir)
+
+# read exodus file
+t = exodus.get_time()
+verts, faces = exodus.get_mesh()
+d = exodus.get_var(var_name="d", timestep=-1) # test nodal var
+stress_11 = exodus.get_var(var_name="stress_11", timestep=-1) # test elemental var
+
+# plot mesh, nodal variable, and elemental variable
+plotter = FEMPlotter(verts, faces)
+plotter.plot(lw=0.1)
+plotter.plot(d)
+plotter.plot(stress_11)
+
 # %%
--- src/utils/post.py
+++ src/utils/post.py
@@ -3,34 +3,77 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.tri as tri
+from matplotlib.collections import PolyCollection
 
 class FEMPlotter:
     """plot variable on FEM mesh (currently only 2D)"""
     
-    def __init__(self, verts, faces, var) -> None:
-        """ verts:[N, dim], 
-            faces:[N, n_nodes_of_element],
-            var: [N, dim]"""
+    def __init__(self, verts, faces) -> None:
+        """ verts:[n_nodes, dim], 
+            faces:[n_elements, n_nodes_of_element],
+            var: [n_nodes, dof] if nodal, [n_elemnts, dof] if elemental"""
         self.verts = verts
         self.faces = faces
-        self.var = var
         
-    def plot(self, dim:int, ax=None, **kwargs):
+    def plot(self, var=None, ax=None, **kwargs):
         """return matplotlib axe"""
         
+        n_nodes = np.size(self.verts, 0)
+        n_elements = np.size(self.faces, 0)
         n_nodes_of_element = np.size(self.faces, 1)
+
+        def quad_to_tri(quad):
+            """convert a quad into 2 traiangles
+            """
+            tris = [quad[:3], [quad[0], quad[2], quad[3]]]
+            return tris
         
+        # plot settings
         if(ax is None):
             fig, ax = plt.subplots()
             ax.set_aspect("equal")
-        
-        if(n_nodes_of_element == 3):
-            # triangle mesh
-            tri_obj = tri.Triangulation(self.verts[:, 0], self.verts[:, 1], self.faces)
-            ax.tripcolor(tri_obj, self.var, cmap=plt.cm.jet, **kwargs)
             
+        # only plot mesh if no variable provided
+        if(var is None):
+            p = PolyCollection([self.verts[face] for face in self.faces], closed=True,
+                               antialiaseds=True, facecolor="none", edgecolor="k", **kwargs)
+            ax.add_collection(p)
+            ax.autoscale()
+            return ax
         else:
-            # quadrilateral mesh
-            pass 
+            clim = (np.min(var), np.max(var))
+        
+        # check variable type
+        if(np.size(var, 0) == n_nodes):
+            var_type = 'nodal'
+        elif(np.size(var, 0) == n_elements):
+            var_type = 'elemental'
+        else:
+            print("Unsupported variable type!")
+            return None
+        
+        if(var_type == 'nodal'):
+            if(n_nodes_of_element == 3):
+                # triangle mesh
+                triangles = self.faces
+            else:
+                tris = []
+                for quad in self.faces:
+                    triangle = quad_to_tri(quad)
+                    tris.extend(triangle)
+                triangles = np.array(tris)
+                
+            # plot nodal value with tripcolor
+            tri_obj = tri.Triangulation(self.verts[:, 0], self.verts[:, 1], triangles)
+            ax.tripcolor(tri_obj, var, clim=clim, cmap=plt.cm.coolwarm, 
+                         shading="gouraud", **kwargs)
+                    
+        else:
+            # plot elemental variable with PolyCollection
+            p = PolyCollection([self.verts[face] for face in self.faces], closed=True, array=var,
+                               cmap=plt.cm.coolwarm, antialiaseds=True, edgecolor="face", **kwargs)
+            p.set_clim(clim)
+            ax.add_collection(p)
+            ax.autoscale()
         
         return ax
