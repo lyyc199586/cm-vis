d167852a433adb62448b39f91395e50f1e0f0648
Yangyaunchen Liu <liuyangyuanchen@outlook.com>
2023-09-15 09:55:34 -0400

    add supports to matplotlib.cm colormaps

--- cm_vis/scheme/colors.py
+++ cm_vis/scheme/colors.py
@@ -6,13 +6,20 @@ from matplotlib.colorbar import ColorbarBase
 class CMColormap:
     '''custom color map class
     '''
-    def __init__(self, colors, clim):
+    def __init__(self, clim, colors=None, cmap=None):
         '''initialize custom colormap
-        colors = ["color1", "color2", ...]
+        colors = ["color1", "color2", ...] or
+        cmap = cmap from matplotlib.cm
         clim = [cmin, cmax]
         '''
         self.norm = Normalize(vmin=clim[0], vmax=clim[1])
-        self.cmap = LinearSegmentedColormap.from_list("", colors)
+        if(cmap is None and colors is None):
+            raise ValueError("Either colors of cmap should be provided!")
+        
+        if(cmap is None):
+            self.cmap = LinearSegmentedColormap.from_list("", colors)
+        else:
+            self.cmap = cmap
         
     def get_cmap(self):
         return self.cmap
